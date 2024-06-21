8b6b4238bf01b32eaac04844ae6ddf2a774b42ba
Maria <mariya-ivanova@list.ru>
2024-06-21 10:17:05 +0200

    the task 47 was solved

---
+++ task47/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+void PrintRandom2DArray(double[,] arrayToPrint){
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++) {
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++) {
+            Console.Write($"{arrayToPrint[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] GenerateRandom2DArray (int m, int n) {
+  double[,] array = new double[m, n];
+  for (int i = 0; i < m; i++){
+    for (int j = 0; j < n; j++){
+        array[i, j] = new Random().NextDouble();
+    }
+  }
+  return array;
+}
+
+int GetNumberAmount(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int rows = GetNumberAmount("Введите желаемое количество строк: ");
+int columns = GetNumberAmount("Введите желаемое количество столбцов: ");
+double[,] resultArray = GenerateRandom2DArray (rows, columns);
+PrintRandom2DArray(resultArray);
